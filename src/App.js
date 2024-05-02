24e065d5255c93e9ecb15ff7d78b6265b4904abd
Shahbaz Ali <aquibahmad345@gmail.com>
2024-05-02 22:09:10 +0530

    Create accordions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion />
     </div>
   );
 }
 
-export default App;
+function Accordion() {
+  return (
+    <div className="accordion">
+      {faqs.map((el, i) => (
+        <Item title={el.title} text={el.text} num={i} />
+      ))}
+    </div>
+  );
+}
+
+function Item({ title, text, num }) {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleIsOpen() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleIsOpen}>
+      <p className="number">{num < 9 ? `0${num + 1}` : `${num + 1}`}</p>
+      <p className="title text">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
